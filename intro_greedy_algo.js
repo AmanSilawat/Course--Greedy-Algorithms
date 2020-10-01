3b3d2298b00b47dddbcff17d5b92e26bbfac21eb
aman <aman.khan.silawat.aks@gmail.com>
2020-10-01 18:20:45 +0530

    Added file: Intro Greedy Algo

---
+++ intro_greedy_algo.js
@@ -0,0 +1,25 @@
+/*
+*Greedy Algorithm*
+
+https://www.geeksforgeeks.org/activity-selection-problem-greedy-algo-1/
+
+Greedy algorithms always make the locally optimal(Best) choice of each stage with  intend of finding gloable optimum.
+
+*Where to use optimal choice?*
+?optimal: minimam cost solution
+?optimal: Max Profit solution
+?optimal: Min Risk solution
+
+every step to go best choice
+
+    B----------------D
+  /                   \
+ /<--10                \
+A                       F
+ \<--5 (choose this)   /
+  \                  /
+    C---------------E
+
+
+
+*/
