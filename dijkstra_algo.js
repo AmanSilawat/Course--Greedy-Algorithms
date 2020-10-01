42d05235c07daf9069e4ea74183aa7480ebe9489
aman <aman.khan.silawat.aks@gmail.com>
2020-10-01 22:36:53 +0530

    Added file: Intro Dijkstras Algo.

---
+++ dijkstra_algo.js
@@ -0,0 +1,17 @@
+/*
+
+Dijkstra’s algorithm is mainly used to find the shortest path from a starting node / point to the target node / point in a weighted graph. the graph must be a weighted. It can be either a directed or undirected graph.
+
+1) Create a set sptSet (shortest path tree set) that keeps track of vertices included in shortest path tree, i.e., whose minimum distance from source is calculated and finalized. Initially, this set is empty.
+2) Assign a distance value to all vertices in the input graph. Initialize all distance values as INFINITE. Assign distance value as 0 for the source vertex so that it is picked first.
+3) While sptSet doesn’t include all vertices
+….a) Pick a vertex u which is not there in sptSet and has minimum distance value.
+….b) Include u to sptSet.
+….c) Update distance value of all adjacent vertices of u. To update the distance values, iterate through all adjacent vertices. For every adjacent vertex v, if sum of distance value of u (from source) and weight of edge u-v, is less than the distance value of v, then update the distance value of v.
+
+*Application of Dijkstra’s Algorithm*
+Social Networking Application
+Telephone Network
+Flight Agenda
+
+*/
